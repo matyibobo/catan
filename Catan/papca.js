3efb223b6bde926696e2399fa774babd574da987
matyibobo <116632311+matyibobo@users.noreply.github.com>
2025-05-23 21:43:55 +0200

    Add files via upload

--- Catan/papca.js
+++ Catan/papca.js
@@ -2,6 +2,9 @@ const grid = document.getElementById("hexGrid");
 const horizontalSpacing = 86.5;
 const verticalSpacing = 75;
 
+const pointHorizontal = 87;
+const pointVertical = 76;
+
 const layout = [
   { count: 3, offset: 2 },
   { count: 4, offset: 1.5 },
@@ -9,10 +12,26 @@ const layout = [
   { count: 4, offset: 1.5 },
   { count: 3, offset: 2 }
 ];
+const pointLayoutOdd = [
+  { count: 3, offset: 2.5 },
+  { count: 4, offset: 2 },
+  { count: 5, offset: 1.5 },
+  { count: 6, offset: 1 },
+  { count: 5, offset: 1.5 },
+  { count: 4, offset: 2 }
+];
+const pointLayoutEven = [
+  { count: 4, offset: 2 },
+  { count: 5, offset: 1.5 },
+  { count: 6, offset: 1 },
+  { count: 5, offset: 1.5 },
+  { count: 4, offset: 2 },
+  { count: 3, offset: 2.5 }
+];
 
 let count = 1;
-
 layout.forEach((row, rowIndex) => {
+  
   for (let i = 0; i < row.count; i++) {
     const wrapper = document.createElement("div");
     wrapper.className = "hex-wrapper";
@@ -27,29 +46,35 @@ layout.forEach((row, rowIndex) => {
     wrapper.style.top = `${top}px`;
 
     wrapper.appendChild(hex);
-    const cx = 45, cy = 52;
-    const radius = 50;
-
-    for (let p = 0; p < 6; p++) {
-      const angle = (Math.PI / 3) * p - Math.PI / 2;
-      const px = cx + radius * Math.cos(angle);
-      const py = cy + radius * Math.sin(angle);
-
-      const point = document.createElement("div");
-      point.className = "point";
-      point.style.left = `${px}px`;
-      point.style.top = `${py}px`;
-
-      // Kattintható pont esemény
-      point.addEventListener("click", () => {
-        point.classList.toggle("clicked");
-        console.log(`Kattintottál egy pontra a(z) ${hex.textContent}. hatszögön (csúcs index: ${p})`);
-      });
-
-      wrapper.appendChild(point);
-    }
-
-
     grid.appendChild(wrapper);
   }
 });
+
+pointLayoutOdd.forEach((row, rowIndex) => {
+  for (let i = 0; i < row.count; i++) {
+    const point = document.createElement("div");
+    point.className = "point";
+
+    const left = (row.offset + i) * pointHorizontal;
+    const top = rowIndex * pointVertical;
+
+    point.style.left = `${left}px`;
+    point.style.top = `${top}px`;
+
+    grid.appendChild(point);
+  }
+})
+pointLayoutEven.forEach((row, rowIndex) => {
+  for (let i = 0; i < row.count; i++) {
+    const point = document.createElement("div");
+    point.className = "point";
+
+    const left = (row.offset + i) * pointHorizontal;
+    const top = rowIndex * pointVertical;
+
+    point.style.left = `${left}px`;
+    point.style.top = `${top + 0.32 * pointVertical}px`;
+
+    grid.appendChild(point);
+  }
+})
